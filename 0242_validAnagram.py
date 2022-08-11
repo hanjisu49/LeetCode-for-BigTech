6127e32c9bee1b1543578d9dbf006886d3f336d7
JiSoo Han <hanjisu49@gmail.com>
2022-08-11 21:28:16 +0900

    build(python) : correct answer and add other best answers

--- 0242_validAnagram.py
+++ 0242_validAnagram.py
@@ -5,10 +5,39 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        temp_s = list(s).sort()
-        temp_t = list(t).sort()
-        
-        if temp_s == temp_t:
-            return True
-        else:
+        if len(s) != len(t):
             return False
+        else:
+            s = list(s)
+            t = list(t)
+            return sorted(s) == sorted(t)
+
+# other best answers - use dictionary
+def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        dic = {}
+        for i in s:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+        
+        for j in t:
+            if j not in dic:
+                return False
+            else:
+                dic[j] -= 1
+        
+        for val in dic.values():
+            if val != 0:
+                return False
+        
+        return True
+
+# python module - collections.Counter 이용 (from collections import Counter 필요) 
+def isAnagram1(self, s, t):
+    return collections.Counter(s) == collections.Counter(t)
