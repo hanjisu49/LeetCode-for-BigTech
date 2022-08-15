1ecf6f85eba95235b261b7fa463ec3fba21f4576
JiSoo Han <hanjisu49@gmail.com>
2022-08-15 16:32:39 +0900

    build(python): add Fibonacci Number

---
+++ 0509_FibonacciNumber.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        fib_array = [0,1]
+        
+        
+        for i in range(2, n+1):
+            num = fib_array[i-1] + fib_array[i-2]
+            fib_array.append(num)
+        return fib_array[n]
